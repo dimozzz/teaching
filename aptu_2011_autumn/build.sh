8d95a2582b05341900214180e049e8f5c5fe23e5
dimozzz <dimoz_88@rambler.ru>
2011-11-11 22:53:33 +0400

    compile

---
+++ aptu_2011_autumn/build.sh
@@ -0,0 +1,18 @@
+mkdir tmp
+for i in *; do
+	if typeset -i "$i" 2> /dev/null; then
+		echo -n
+	else
+		if test -d "$i"; then
+			cp main.tex "$i"
+			cd "$i"
+			pdflatex --output-directory ../tmp -jobname "$i" main.tex
+			pdflatex --output-directory ../tmp -jobname "$i" main.tex
+			cd ..	
+		fi
+	fi
+done
+
+cd tmp
+rm *.log
+rm *.aux
