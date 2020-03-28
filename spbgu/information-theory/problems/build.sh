5f790077ef6eacd8c0d33caf25913f13774260f8
Dmitry Sokolov <sokolov.dmt@gmail.com>
2020-03-29 02:15:04 +0300

    1

---
+++ spbgu/information-theory/problems/build.sh
@@ -0,0 +1,14 @@
+dir=pdf
+compiler=pdflatex
+
+mkdir $dir
+touch $dir/number.txt
+cd series
+for i in *.tex; do
+	$compiler --output-directory ../$dir -jobname "${i::-4}" $i
+done
+
+cd ../$dir
+rm *.log
+rm *.aux
+rm *.txt
