7692494bc579d4b122c78ef5457182c0858378d9
Dmitry Sokolov <sokolov.dmt@gmail.com>
2020-06-11 23:01:17 +0300

    1

---
+++ spbgu/information-theory/students-notes/build.sh
@@ -0,0 +1,11 @@
+compiler=xelatex
+dir=pdf
+file=notes
+mkdir $dir
+$compiler --output-directory $dir $file.tex
+cp references.bib $dir
+cd $dir
+biber notes
+cd ..
+$compiler --output-directory $dir $file.tex
+$compiler --output-directory $dir $file.tex
