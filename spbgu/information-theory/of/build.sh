b4995546d3640352da7229b4dfb047e756b6a7cc
Dmitry Sokolov <sokolov.dmt@gmail.com>
2020-07-18 21:36:53 +0300

    1

--- spbgu/information-theory/of/build.sh
+++ spbgu/information-theory/of/build.sh
@@ -1,12 +1,14 @@
 dir=pdf
 files=("information-theory")
+compiler=xelatex
+
 mkdir $dir
 for i in "${files[@]}"; do
-    pdflatex --output-directory $dir $i.tex
+    $compiler --output-directory $dir $i.tex
     cp main.bib $dir
     cd $dir
     biber $i
     cd ..
-    pdflatex --output-directory $dir $i.tex
-    pdflatex --output-directory $dir $i.tex
+    $compiler --output-directory $dir $i.tex
+    $compiler --output-directory $dir $i.tex
 done
