2364d3ed9087089a559028a796c924376a343678
Dmitry Sokolov <sokolov.dmt@gmail.com>
2020-07-26 22:33:04 +0300

    1

--- utils/src/db-build.sh
+++ utils/src/db-build.sh
@@ -2,11 +2,12 @@
 
 
 db-build(){
+    compiler=pdflatex
     file=$1
     dir=$2
-    if ! pdflatex --output-directory "$dir" -halt-on-error -interaction=nonstopmode "$file" >/dev/null; then
-        if !  pdflatex --output-directory "$dir" -halt-on-error -interaction=nonstopmode "$file" >/dev/null; then
-            pdflatex --output-directory "$dir" -halt-on-error -interaction=nonstopmode "$file" || true
+    if ! $compiler --output-directory "$dir" -halt-on-error -interaction=nonstopmode "$file" >/dev/null; then
+        if ! $compiler --output-directory "$dir" -halt-on-error -interaction=nonstopmode "$file" >/dev/null; then
+            $compiler --output-directory "$dir" -halt-on-error -interaction=nonstopmode "$file" || true
             echo "Ошибка в сборке $file."
             exit 1
         fi
