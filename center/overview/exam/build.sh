8520224be99d05b36949d8e6874b87dedcd5c074
Dmitry Sokolov <sokolov.dmt@gmail.com>
2020-07-30 21:05:31 +0300

    1

--- center/overview/exam/build.sh
+++ center/overview/exam/build.sh
@@ -1,64 +1,11 @@
-#!/bin/bash
+dir=pdf
+compiler=xelatex
 
-IFS=$'\n'
-file=curseries.tex
-mkdir tmp
-declare -A tasks
-cd tmp
-cp ../main.tex .
+mkdir $dir
+$compiler --output-directory $dir main.tex
 
-
-ser=1
-task=1
-title=true
-break=true
-
-echo -e "\input{../rules.tex}" >> $file
-
-for line in `grep '[^[:blank:]]' ../series.txt`; do
-	if [ $title = true ]; then
-		echo $title
-		echo -e "\setmathstyle{АУ}{$line}{CC, 5 курс}\n" >> $file
-		title=false
-		continue
-	fi
-	if [ $line = "F" ]; then		
-		pdflatex -jobname "$(printf "%02d" $ser)" main.tex
-		let "ser += 1"
-		break=true
-		title=true
-		rm $file
-	else
-		for i in `echo $line | tr ',' '\n' | tr -d ' '`; do
-			if [[ ${i:0:1} == "d" ]] ; then
-				echo "\begin{definition}" >> $file
-				echo -e "\input{../../base/definitions/"${i:1:15}".tex}" >> $file
-				echo "\end{definition}" >> $file
-				continue
-			fi
-			if [ "${tasks[$i]}" = "" ]; then
-				tasks[$i]=$task
-				let "task += 1"
-			else
-				if [ $break = true ]; then
-					echo "\breakline" >> $file
-					echo >> $file
-				fi
-				break=false
-			fi
-			echo -e "\setcounter{curtask}{${tasks["$i"]}}\n\n" >> $file
-			echo "\begin{task}" >> $file
-			echo -e "\input{../../base/problems/"$i".tex}" >> $file
-			echo "\end{task}" >> $file
-		done
-	fi
-done
-
-rm main.tex
-rm $file
-
+cd ../$dir
 rm *.log
 rm *.out
 rm *.aux
 rm *.txt
-
