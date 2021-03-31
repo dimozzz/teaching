7ae6e0937c9338af3992106adf97be29ed71f116
Dmitry Sokolov <sokolov.dmt@gmail.com>
2021-03-31 22:03:39 +0300

    1

---
+++ spbgu/sat-solving/announce/build.sh
@@ -0,0 +1,13 @@
+dir=pdf
+files=("ru" "en")
+compiler=xelatex
+mkdir $dir
+for i in "${files[@]}"; do
+    $compiler --output-directory $dir $i.tex
+    cp references.bib $dir
+    cd $dir
+    biber $i
+    cd ..
+    $compiler --output-directory $dir $i.tex
+    $compiler --output-directory $dir $i.tex
+done
